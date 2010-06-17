2f1f2b25c9d4db7b402946b7256407a7b114f1ec
Vedran Mileti? <rivanvx@gmail.com>
2010-06-17 11:25:56 +0200

    Remove some trailing whitespace.

--- doc/themeexamples/beamerthememakeexamples.sh
+++ doc/themeexamples/beamerthememakeexamples.sh
@@ -4,7 +4,7 @@ for i in \
   Antibes Berkeley Berlin Copenhagen Darmstadt \
   Dresden Goettingen Hannover Ilmenau JuanLesPins \
   Luebeck Malmoe Marburg Montpellier PaloAlto \
-  Pittsburgh Rochester Singapore Szeged Warsaw; do 
+  Pittsburgh Rochester Singapore Szeged Warsaw; do
   pdflatex \\def\\theme\{"$i"\}\\input beamerthemeexample.tex;
   mv beamerthemeexample.pdf beameruserguideexample"$i".pdf;
 done
