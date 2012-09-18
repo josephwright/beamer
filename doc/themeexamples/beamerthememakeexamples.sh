8e37175f439ff371b9516f1f1d00911cf1275420
Joseph Wright <joseph.wright@morningstar2.co.uk>
2012-09-18 21:19:03 +0100

    Update theme Makefile (fixes issue #217)

--- doc/themeexamples/beamerthememakeexamples.sh
+++ doc/themeexamples/beamerthememakeexamples.sh
@@ -1,11 +1,11 @@
-#$/bin/sh
-
+#!/bin/sh
+ 
 for i in \
-  Antibes Berkeley Berlin Copenhagen Darmstadt \
-  Dresden Goettingen Hannover Ilmenau JuanLesPins \
-  Luebeck Malmoe Marburg Montpellier PaloAlto \
-  Pittsburgh Rochester Singapore Szeged Warsaw; do
-  pdflatex \\def\\theme\{"$i"\}\\input beamerthemeexample.tex;
-  mv beamerthemeexample.pdf beameruserguideexample"$i".pdf;
+  AnnArbor Antibes Bergen Berkeley Berlin Boadilla boxes \
+  CambridgeUS Copenhagen Darmstadt Dresden EastLansing \
+  Frankfurt Goettingen Hannover Ilmenau JuanLesPins \
+  Luebeck Madrid Malmoe Marburg Montpellier PaloAlto \
+  Pittsburgh Rochester Singapore Szeged Warsaw; do (
+  pdflatex \\def\\themename\{"$i"\}\\input beamerthemeexample.tex;
+  mv beamerthemeexample.pdf beameruserguideexample"$i".pdf; )
 done
-
