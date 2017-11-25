8ac27510422bdffa62d91894602c385b76b4befc
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-11-25 15:58:00 +0000

    Add some support notes for building on Linux
    At least until we get a cross-platform build arrangement, releases are
    done from a Linux VM at the moment. To avoid having to work out the
    detail each time, store all of the useful info in some support files.

---
+++ support/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+# See if there is a cached verson of TL available
+export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+if ! command -v texlua > /dev/null; then
+  # Obtain TeX Live
+  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+  tar -xzf install-tl-unx.tar.gz
+  cd install-tl-20*
+
+  # Install a minimal system
+  ./install-tl --profile=../texlive.profile
+
+  cd ..
+fi
+
+tlmgr install \
+  babel-german \
+  cmap \
+  courier \
+  ec \
+  etoolbox \
+  extsizes \
+  helvetic \
+  listings \
+  lm \
+  pgf \
+  rsfs \
+  symbol \
+  times \
+  xcolor \
+  yfonts \
+  zapfding
+
+# Keep no backups (not required, simply makes cache bigger)
+tlmgr option -- autobackup 0
+
+# Update the TL install but add nothing new
+tlmgr update --self --all --no-auto-install
+
+mkdir -p ~/texmf/tex/latex/
+push ~/texmf/tex/latex/
+ln -s ~/beamer/
+popd
