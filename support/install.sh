3ef2284959bc73129743fbbd963e396531d5d2c9
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-12-07 20:43:39 +0000

    TEXINPUTS needs to be recursive

--- support/install.sh
+++ support/install.sh
@@ -39,4 +39,4 @@ tlmgr option -- autobackup 0
 # Update the TL install but add nothing new
 tlmgr update --self --all --no-auto-install
 
-export TEXINPUTS=`pwd`/..:$TEXINPUTS
+export TEXINPUTS=`pwd`/..//:$TEXINPUTS
