5212cb1467ef75ce1e94ea48f02f0cc368fad3a3
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-12-09 20:21:47 +0000

    Set up demo compilation
    This currently needs the dev version of l3build.

--- build.lua
+++ build.lua
@@ -29,6 +29,60 @@ cleanfiles = {"beameruserguide.pdf", "*.zip"}
 -- Release a TDS-style zip
 packtdszip = true
 
+-- Custom data for the theme demos
+themes =
+  {
+    core = 
+      {
+        "AnnArbor", "Antibes", "Berkeley", "Berlin", "Bergen", "Boadilla",
+        "Copenhagen", "Darmstadt", "Dresden", "EastLansing", "Frankfurt",
+        "Goettingen", "Hannover", "Ilmenau", "JuanLesPins", "Luebeck",
+        "Malmoe", "Madrid", "Marburg", "Montpellier", "PaloAlto",
+        "Pittsburgh", "Rochester", "Singapore", "Szeged", "Warsaw",
+        "CambridgeUS", "default", "boxes"
+      },
+    font = 
+      {
+        "default", "serif", "structurebold", "structureitalicserif",
+        "structuresmallcapsserif"
+      },
+    color =
+      {
+        "default", "crane", "albatross", "seahorse", "whale", "dolphin",
+        "rose", "orchid", "sidebartab", "lily", "structure", "dove", "seagull",
+        "beetle", "fly", "wolverine", "spruce", "beaver", "monarca"
+      },
+    outer =
+      {
+        "default", "infolines", "miniframes", "shadow", "sidebar",
+        "smoothbars", "smoothtree", "split", "tree"
+      },
+    inner = {"default", "circles", "rectangles", "rounded", "inmargin"}
+  }
+
+function typeset_demo_tasks()
+  local errorlevel = 0
+  for type,themelist in pairs(themes) do
+    local themetype = ""
+    if  type~= "core" then themetype = tostring(type) end
+    local name = "beamer" .. themetype .. "themeexample.tex"
+    for _,theme in pairs(themelist) do
+      errorlevel = errorlevel + runtool(
+        "", ".",
+        "TEXINPUTS",
+        "pdflatex \\def\\themename{" .. theme .. "}"
+          .. "\\input " .. name .. "\""
+      )
+      if errorlevel == 0 then
+        ren(typesetdir,
+          string.gsub(name, "%.tex$", ".pdf"),
+          "beamerug" .. themetype .. "theme" .. theme .. ".pdf")
+      end
+    end
+  end
+  return errorlevel
+end
+
 -- Find and run the build system
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
